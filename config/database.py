55cf8e105b3e8a208b97b6105bf1bb871a173c84
Gian franco baeza <gianb04@gmail.com>
2025-08-25 19:59:09 -0300

    seeds

--- config/database.py
+++ config/database.py
@@ -7,19 +7,39 @@ import os
 # Cargar variables de entorno
 load_dotenv()
 
-# URL de conexión a PostgreSQL desde variable de entorno
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
 
-if not SQLALCHEMY_DATABASE_URL:
+# Construct the SQLAlchemy connection string
+DATABASE_URL = (
+    f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+)
+
+
+if not DATABASE_URL:
     raise ValueError(
         "❌ Variable de entorno DATABASE_URL no encontrada. Verifica tu archivo .env"
     )
 
 # Motor de conexión
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+)
 
 # Crear sesiones
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base para modelos
 Base = declarative_base()
+
+
+# Test the connection
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Failed to connect: {e}")
